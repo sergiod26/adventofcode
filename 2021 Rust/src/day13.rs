aad503e9dcfbb9cf093dd02b92af43e94a7bf936
Sergio Diaz <sergio.diaz@pdq.com>
2021-12-13 10:42:55 -0600

    day13: clean up

--- 2021 Rust/src/day13.rs
+++ 2021 Rust/src/day13.rs
@@ -1,56 +1,91 @@
+pub struct Data {
+    points: Vec<(usize, usize)>,
+    folds: Vec<(String, usize)>,
+}
+
 #[aoc_generator(day13)]
-pub fn input_generator(input: &str) -> Vec<Vec<usize>> {
-    input
-        .lines()
-        .map(|l| l.split(',').map(|x| x.parse::<usize>().unwrap()).collect())
-        .collect()
+pub fn input_generator(input: &str) -> Data {
+    let mut input = input.lines();
+    let mut points: Vec<(usize, usize)> = vec![];
+
+    for line in &mut input {
+        if line.is_empty() {
+            break;
+        }
+        let point: Vec<usize> = line
+            .split(',')
+            .map(|x| x.parse::<usize>().unwrap())
+            .collect();
+        points.push((point[0], point[1]));
+    }
+
+    let mut folds = vec![];
+    for line in input {
+        folds.push(line);
+    }
+
+    let folds = folds
+        .iter()
+        .map(|e| {
+            let tmp: Vec<&str> = e.split('=').collect();
+            (
+                tmp[0].to_owned().replace("fold along ", ""),
+                tmp[1].parse::<usize>().unwrap(),
+            )
+        })
+        .collect();
+
+    Data { points, folds }
 }
 
 #[aoc(day13, part1)]
-pub fn part1(input: &[Vec<usize>]) -> usize {
-    let fold = 655;
-    let result = fold_x(input, fold);
+pub fn part1(input: &Data) -> usize {
+    let result = fold(input.points.clone(), input.folds[0].clone());
     result.len()
 }
+
 #[aoc(day13, part2)]
-pub fn part2(input: &[Vec<usize>]) -> usize {
-    let result = fold_x(input, 655);
-    let result = fold_y(&result, 447);
-    let result = fold_x(&result, 327);
-    let result = fold_y(&result, 223);
-    let result = fold_x(&result, 163);
-    let result = fold_y(&result, 111);
-    let result = fold_x(&result, 81);
-    let result = fold_y(&result, 55);
-    let result = fold_x(&result, 40);
-    let result = fold_y(&result, 27);
-    let result = fold_y(&result, 13);
-    let result = fold_y(&result, 6);
+pub fn part2(input: &Data) -> usize {
+    let mut result = input.points.clone();
+    input
+        .folds
+        .iter()
+        .for_each(|f| result = fold(result.clone(), f.clone()));
 
-    //println!("{:?}", result);
+    display(result.clone());
 
-    let mut matrix = vec![vec!['.'; 50]; 50];
+    result.len()
+}
 
-    result.iter().for_each(|x| matrix[x[1]][x[0]] = '#');
-
+fn display(points: Vec<(usize, usize)>) {
+    let rows = points.iter().map(|(x, _)| x).max().unwrap() + 1;
+    let cols = points.iter().map(|(_, y)| y).max().unwrap() + 1;
+    let mut matrix = vec![vec![' '; rows]; cols];
+    points.iter().for_each(|x| matrix[x.1][x.0] = '#');
     for row in matrix {
         for cell in row {
             print!("{}", cell);
         }
-        println!("");
+        println!();
     }
-
-    0
 }
-fn fold_x(input: &[Vec<usize>], fold: usize) -> Vec<Vec<usize>> {
-    let mut result: Vec<Vec<usize>> = input
+
+fn fold(points: Vec<(usize, usize)>, (dir, point): (String, usize)) -> Vec<(usize, usize)> {
+    match dir.as_str() {
+        "x" => fold_x(points, point),
+        _ => fold_y(points, point),
+    }
+}
+
+fn fold_x(input: Vec<(usize, usize)>, fold: usize) -> Vec<(usize, usize)> {
+    let mut result: Vec<(usize, usize)> = input
         .iter()
-        .filter(|x| x[0] != fold)
+        .filter(|x| x.0 != fold)
         .map(|x| {
-            if x[0] > fold {
-                vec![(fold - (x[0] % fold)) % fold, x[1]]
+            if x.0 > fold {
+                (fold - (x.0 - fold), x.1)
             } else {
-                vec![x[0], x[1]]
+                (x.0, x.1)
             }
         })
         .collect();
@@ -59,15 +94,15 @@ fn fold_x(input: &[Vec<usize>], fold: usize) -> Vec<Vec<usize>> {
     result
 }
 
-fn fold_y(input: &[Vec<usize>], fold: usize) -> Vec<Vec<usize>> {
-    let mut result: Vec<Vec<usize>> = input
+fn fold_y(input: Vec<(usize, usize)>, fold: usize) -> Vec<(usize, usize)> {
+    let mut result: Vec<(usize, usize)> = input
         .iter()
-        .filter(|x| x[1] != fold)
+        .filter(|x| x.1 != fold)
         .map(|x| {
-            if x[1] > fold {
-                vec![x[0], (fold - (x[1] % fold)) % fold]
+            if x.1 > fold {
+                (x.0, fold - (x.1 - fold))
             } else {
-                vec![x[0], x[1]]
+                (x.0, x.1)
             }
         })
         .collect();
@@ -97,7 +132,11 @@ mod tests {
 1,10
 2,14
 8,10
-9,0";
+9,0
+
+fold along y=7
+fold along x=5";
+
     #[test]
     pub fn part1_test() {
         let result = part1(&input_generator(INPUT));
@@ -107,6 +146,6 @@ mod tests {
     #[test]
     pub fn part2_test() {
         let result = part2(&input_generator(INPUT));
-        assert_eq!(result, 17);
+        assert_eq!(result, 16);
     }
 }
