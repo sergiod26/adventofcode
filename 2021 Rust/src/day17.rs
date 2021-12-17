c7115d05db1561e3e04adca64ab3af3399a7d96c
Sergio Diaz <sergio.diaz@pdq.com>
2021-12-17 09:50:41 -0600

    day17: reading input, still brute force!

--- 2021 Rust/src/day17.rs
+++ 2021 Rust/src/day17.rs
@@ -1,19 +1,26 @@
-// #[aoc_generator(day17)]
-// pub fn input_generator(input: &str) -> Vec<i32> {
-//     todo!();
-// }
+#[aoc_generator(day17)]
+pub fn input_generator(input: &str) -> ((i32, i32), (i32, i32)) {
+    let vec: Vec<i32> = input
+        .trim()
+        .replace("target area: x=", "")
+        .replace(" y=", "")
+        .split(',')
+        .flat_map(|e| e.split(".."))
+        .map(|n| n.parse::<i32>().unwrap())
+        .collect();
+
+    ((vec[0], vec[1]), (vec[2], vec[3]))
+}
 
 #[aoc(day17, part1)]
-pub fn part1(_input: &str) -> i32 {
-    let target_x = (230, 283);
-    let target_y = (-107, -57);
+pub fn part1(input: &((i32, i32), (i32, i32))) -> i32 {
+    let &(target_x, target_y) = input;
     brute_force(target_x, target_y).into_iter().max().unwrap()
 }
 
 #[aoc(day17, part2)]
-pub fn part2(_input: &str) -> usize {
-    let target_x = (230, 283);
-    let target_y = (-107, -57);
+pub fn part2(input: &((i32, i32), (i32, i32))) -> usize {
+    let &(target_x, target_y) = input;
     brute_force(target_x, target_y).len()
 }
 
@@ -21,6 +28,7 @@ fn brute_force(target_x: (i32, i32), target_y: (i32, i32)) -> Vec<i32> {
     let mut result = vec![];
     for x in 0..=target_x.1 {
         for y in target_y.0..500 {
+            //why 500 you ask? just because :)
             let tmp = path((x, y), target_x, target_y);
             if tmp != i32::MIN {
                 result.push(tmp);
@@ -62,16 +70,16 @@ fn path(velocity: (i32, i32), target_x: (i32, i32), target_y: (i32, i32)) -> i32
 mod tests {
     use super::*;
 
-    const INPUT: &str = "3,4,3,1,2";
+    const INPUT: &str = "target area: x=20..30, y=-10..-5";
     #[test]
     pub fn part1_test() {
-        let result = part1(INPUT);
-        assert_eq!(result, 5671);
+        let result = part1(&input_generator(INPUT));
+        assert_eq!(result, 45);
     }
 
     #[test]
     pub fn part2_test() {
-        let result = part2(INPUT);
-        assert_eq!(result, 4556);
+        let result = part2(&input_generator(INPUT));
+        assert_eq!(result, 112);
     }
 }
