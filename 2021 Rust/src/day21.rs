748c546e9e57877a727ce144037a21270294eb19
Sergio Diaz <sergio.diaz@pdq.com>
2021-12-21 14:58:23 -0600

    day21: refactor

--- 2021 Rust/src/day21.rs
+++ 2021 Rust/src/day21.rs
@@ -44,68 +44,61 @@ pub fn part1(input: &[usize]) -> usize {
 #[aoc(day21, part2)]
 pub fn part2(input: &[usize]) -> usize {
     let mut cache = HashMap::new();
-    let result = play(true, input[0], 0, input[1], 0, &mut cache);
+    let players = vec![
+        Player {
+            position: input[0],
+            score: 0,
+        },
+        Player {
+            position: input[1],
+            score: 0,
+        },
+    ];
+    let result = play(0, players, &mut cache);
     *vec![result.0, result.1].iter().max().unwrap()
 }
 
+#[derive(PartialEq, Eq, Hash, Clone)]
+pub struct Player {
+    position: usize,
+    score: usize,
+}
+
 fn play(
-    is_player1: bool,
-    pos1: usize,
-    score1: usize,
-    pos2: usize,
-    score2: usize,
-    cache: &mut HashMap<(bool, usize, usize, usize, usize), (usize, usize)>,
+    current: usize,
+    players: Vec<Player>,
+    cache: &mut HashMap<(usize, Vec<Player>), (usize, usize)>,
 ) -> (usize, usize) {
     //Multiple universes will have the same configuration... so remember solutions and skip calculations
-    if let Some(res) = cache.get(&(is_player1, pos1, score1, pos2, score2)) {
+    if let Some(res) = cache.get(&(current, players.clone())) {
         return *res;
     }
-
-    if score1 >= 21 {
-        *cache
-            .entry((true, pos1, score1, pos2, score2))
-            .or_insert((1, 0)) = (1, 0);
+    if players[0].score >= 21 {
+        *cache.entry((0, players)).or_insert((1, 0)) = (1, 0);
         return (1, 0);
     }
-    if score2 >= 21 {
-        *cache
-            .entry((false, pos1, score1, pos2, score2))
-            .or_insert((0, 1)) = (0, 1);
+    if players[1].score >= 21 {
+        *cache.entry((1, players)).or_insert((0, 1)) = (0, 1);
         return (0, 1);
     }
 
     let mut total_wins = (0, 0);
-    let position;
-    let score;
-    if is_player1 {
-        position = pos1;
-        score = score1;
-    } else {
-        position = pos2;
-        score = score2;
-    }
-
     for d1 in 1..=3 {
         for d2 in 1..=3 {
             for d3 in 1..=3 {
                 let dice = d1 + d2 + d3;
-                let pos = ((position + dice - 1) % 10) + 1;
-                let points = score + pos;
+                let position = ((players[current].position + dice - 1) % 10) + 1;
+                let score = players[current].score + position;
 
-                let rec;
-                if is_player1 {
-                    rec = play(false, pos, points, pos2, score2, cache);
-                } else {
-                    rec = play(true, pos1, score1, pos, points, cache);
-                }
+                let mut updated = players.clone();
+                updated[current] = Player { position, score };
+                let rec = play((current + 1) % 2, updated, cache);
                 total_wins = (total_wins.0 + rec.0, total_wins.1 + rec.1);
             }
         }
     }
-    *cache
-        .entry((is_player1, pos1, score1, pos2, score2))
-        .or_insert(total_wins) = total_wins;
-    return total_wins;
+    *cache.entry((current, players)).or_insert(total_wins) = total_wins;
+    total_wins
 }
 
 #[cfg(test)]
