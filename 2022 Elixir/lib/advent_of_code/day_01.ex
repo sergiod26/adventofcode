569565078c042a9130c45f36f6dbdc521b16ae2b
Sergio Diaz <sergio.diaz@pdq.com>
2022-12-01 08:02:12 -0600

    2022 day 1

--- 2022 Elixir/lib/advent_of_code/day_01.ex
+++ 2022 Elixir/lib/advent_of_code/day_01.ex
@@ -1,7 +1,25 @@
 defmodule AdventOfCode.Day01 do
   def part1(args) do
+    args
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn l ->
+      String.split(l, "\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end)
+    |> Enum.max()
   end
 
   def part2(args) do
+    args
+    |> String.split("\n\n", trim: true)
+    |> Enum.map(fn l ->
+      String.split(l, "\n", trim: true)
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.sum()
+    end)
+    |> Enum.sort(:desc)
+    |> Enum.take(3)
+    |> Enum.sum()
   end
 end
