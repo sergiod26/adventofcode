7ad47118c0ae38a13b6b2a1d1ffe4768c57eb454
Sergio Diaz <sergio.diaz@pdq.com>
2022-12-03 09:22:45 -0600

    day 3... ugly? yes... functional? maybe!

--- 2022 Elixir/lib/advent_of_code/day_03.ex
+++ 2022 Elixir/lib/advent_of_code/day_03.ex
@@ -1,7 +1,37 @@
 defmodule AdventOfCode.Day03 do
   def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn x ->
+      list = String.to_charlist(x)
+      [c1, c2] = Enum.chunk_every(list, div(length(list), 2))
+
+      # should I google for a real `intersect` yeah... does this work instead?! I think so
+      tmp = c1 -- c2
+      (c1 -- tmp) |> Enum.uniq() |> to_string
+    end)
+    |> Enum.map(fn x ->
+      <<v::utf8>> = x
+      if(v < 97, do: v - 38, else: v - 96)
+    end)
+    |> Enum.sum()
   end
 
   def part2(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_charlist/1)
+    |> Enum.chunk_every(3)
+    |> Enum.map(fn [a, b, c] ->
+      x1 = a -- b
+      x2 = a -- x1
+      x3 = x2 -- c
+      (x2 -- x3) |> Enum.uniq() |> to_string
+    end)
+    |> Enum.map(fn x ->
+      <<v::utf8>> = x
+      if(v < 97, do: v - 38, else: v - 96)
+    end)
+    |> Enum.sum()
   end
 end
