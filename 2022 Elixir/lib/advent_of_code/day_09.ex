81d8691f6e7d633f2ec96b31d195354fe779fefb
Sergio Diaz <sergio.diaz@pdq.com>
2022-12-09 13:25:34 -0600

    2022, day 9

--- 2022 Elixir/lib/advent_of_code/day_09.ex
+++ 2022 Elixir/lib/advent_of_code/day_09.ex
@@ -1,7 +1,82 @@
+# Lots of iterations
+# move_tail is the basic rules
+# iterate over the movements, saving a set of the positions where the tail has been
+#  remember the last position for both, tail and head (on part 2, extend to have a head and 8 nodes behaving like tails)
+
 defmodule AdventOfCode.Day09 do
   def part1(args) do
+    {_, _, final_path} =
+      args
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn l ->
+        [dir, qt] = String.split(l, " ")
+        {dir, String.to_integer(qt)}
+      end)
+      # {head, tail, tail's path (mapset, ignore dupes)}
+      |> Enum.reduce({{0, 0}, {0, 0}, MapSet.new([])}, fn {dir, qt}, acc ->
+        1..qt
+        |> Enum.reduce(acc, fn _, {head, tail, path} ->
+          new_head = move_head(dir, head)
+          new_tail = move_tail(new_head, tail)
+          {new_head, new_tail, MapSet.put(path, new_tail)}
+        end)
+      end)
+
+    MapSet.size(final_path)
   end
 
   def part2(args) do
+    {_, final_path} =
+      args
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn l ->
+        [dir, qt] = String.split(l, " ")
+        {dir, String.to_integer(qt)}
+      end)
+      # Use a map with the positions of every knot
+      |> Enum.reduce({0..9 |> Map.new(&{&1, {0, 0}}), MapSet.new([])}, fn {dir, qt}, acc ->
+        1..qt
+        |> Enum.reduce(acc, fn _, {rope, path} ->
+          rope = %{rope | 0 => move_head(dir, Map.get(rope, 0))}
+
+          # There is probably a better way...
+          # but just ignore the real tail in the loop
+          # since we need to use it later for the path
+          rope =
+            0..7
+            |> Enum.reduce(rope, fn ix, acc ->
+              %{acc | (ix + 1) => move_tail(Map.get(acc, ix), Map.get(acc, ix + 1))}
+            end)
+
+          # Once more, same as before but for the "real tail"
+          new_tail = move_tail(Map.get(rope, 8), Map.get(rope, 9))
+          {%{rope | 9 => new_tail}, MapSet.put(path, new_tail)}
+        end)
+      end)
+
+    MapSet.size(final_path)
+  end
+
+  defp move_head(dir, {hx, hy}) do
+    case dir do
+      "R" -> {hx + 1, hy}
+      "L" -> {hx - 1, hy}
+      "U" -> {hx, hy + 1}
+      "D" -> {hx, hy - 1}
+    end
+  end
+
+  defp move_tail({hx, hy}, {tx, ty}) do
+    cond do
+      hx - tx > 1 && hy - ty > 1 -> {tx + 1, ty + 1}
+      hx - tx < -1 && hy - ty < -1 -> {tx - 1, ty - 1}
+      hx - tx > 1 && hy - ty < -1 -> {tx + 1, ty - 1}
+      hx - tx < -1 && hy - ty > 1 -> {tx - 1, ty + 1}
+      hx - tx > 1 -> {tx + 1, hy}
+      hx - tx < -1 -> {tx - 1, hy}
+      hy - ty > 1 -> {hx, ty + 1}
+      hy - ty < -1 -> {hx, ty - 1}
+      true -> {tx, ty}
+    end
   end
 end
--- 2022 Elixir/test/advent_of_code/day_09_test.exs
+++ 2022 Elixir/test/advent_of_code/day_09_test.exs
@@ -3,19 +3,32 @@ defmodule AdventOfCode.Day09Test do
 
   import AdventOfCode.Day09
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2"
     result = part1(input)
 
-    assert result
+    assert result == 13
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "R 5
+U 8
+L 8
+D 3
+R 17
+D 10
+L 25
+U 20"
+
     result = part2(input)
 
-    assert result
+    assert result == 36
   end
 end
