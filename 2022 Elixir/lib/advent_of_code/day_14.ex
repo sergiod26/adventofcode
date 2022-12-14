510657010f2f08cf362ae4bb6bedc166dcf4d334
Sergio Diaz <sergio.diaz@pdq.com>
2022-12-14 17:17:12 -0600

    2022, day 14: cleaning up

--- 2022 Elixir/lib/advent_of_code/day_14.ex
+++ 2022 Elixir/lib/advent_of_code/day_14.ex
@@ -3,13 +3,13 @@
 
 defmodule AdventOfCode.Day14 do
   def part1(args) do
-    {cave, min_x, max_x, max_y} = parse(args)
-    simulate(cave, min_x, max_x, max_y)
+    {cave, max_y} = parse(args)
+    simulate(cave, max_y)
   end
 
   def part2(args) do
-    {cave, min_x, max_x, max_y} = parse(args)
-    simulate(cave, min_x, max_x, max_y + 1, true)
+    {cave, max_y} = parse(args)
+    simulate(cave, max_y + 1, true)
   end
 
   # Returns a list of coordinates with all rock spaces
@@ -33,18 +33,17 @@ defmodule AdventOfCode.Day14 do
       end)
       |> Map.new(&{&1, "#"})
 
-    {{min_x, _}, {max_x, _}} = Map.keys(cave) |> Enum.min_max_by(fn {x, _} -> x end)
     {_, max_y} = Map.keys(cave) |> Enum.max_by(fn {_, y} -> y end)
 
-    {cave, min_x, max_x, max_y}
+    {cave, max_y}
   end
 
   @start {500, 0}
 
-  defp simulate(cave, min_x, max_x, max_y, infinite_floor \\ false) do
+  defp simulate(cave, max_y, infinite_floor \\ false) do
     Stream.iterate(0, &(&1 + 1))
     |> Enum.reduce_while(cave, fn _, acc ->
-      {next, new_sand} = fall(acc, @start, {min_x, max_x}, max_y, infinite_floor)
+      {next, new_sand} = fall(@start, acc, max_y, infinite_floor)
 
       case next do
         :cont -> {next, Map.put(acc, new_sand, "o")}
@@ -55,14 +54,16 @@ defmodule AdventOfCode.Day14 do
     |> Enum.count(&(&1 == "o"))
   end
 
-  defp fall(cave, {x, y}, {min, max}, max_y, infinite_floor) do
+  defp fall({x, y}, cave, max_y, infinite_floor) do
+    {down, left, right} = {{x, y + 1}, {x - 1, y + 1}, {x + 1, y + 1}}
+
     cond do
       cave[@start] != nil -> {:halt, {x, y}}
       infinite_floor && y == max_y -> {:cont, {x, y}}
       y > max_y -> {:halt, {x, y}}
-      cave[{x, y + 1}] == nil -> fall(cave, {x, y + 1}, {min, max}, max_y, infinite_floor)
-      cave[{x - 1, y + 1}] == nil -> fall(cave, {x - 1, y + 1}, {min, max}, max_y, infinite_floor)
-      cave[{x + 1, y + 1}] == nil -> fall(cave, {x + 1, y + 1}, {min, max}, max_y, infinite_floor)
+      !cave[down] -> fall(down, cave, max_y, infinite_floor)
+      !cave[left] -> fall(left, cave, max_y, infinite_floor)
+      !cave[right] -> fall(right, cave, max_y, infinite_floor)
       true -> {:cont, {x, y}}
     end
   end
