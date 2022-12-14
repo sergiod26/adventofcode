5741147bd1859ace708653e45e7fc1fa33efef61
Sergio Diaz <sergio.diaz@pdq.com>
2022-12-14 13:59:47 -0600

    2022, day 14: more refactor, less duplication, maybe not as clear

--- 2022 Elixir/lib/advent_of_code/day_14.ex
+++ 2022 Elixir/lib/advent_of_code/day_14.ex
@@ -2,6 +2,16 @@
 # from the smallest x to the highest one (like in the example)
 
 defmodule AdventOfCode.Day14 do
+  def part1(args) do
+    {cave, min_x, max_x, max_y} = parse(args)
+    simulate(cave, min_x, max_x, max_y)
+  end
+
+  def part2(args) do
+    {cave, min_x, max_x, max_y} = parse(args)
+    simulate(cave, min_x, max_x, max_y + 1, true)
+  end
+
   # Returns a list of coordinates with all rock spaces
   # %{{494, 9} => "#",{495, 9} => "#",{496, 6} => "#",...}
   defp parse(args) do
@@ -11,60 +21,43 @@ defmodule AdventOfCode.Day14 do
       |> Enum.flat_map(fn l ->
         String.split(l, " -> ", trim: true)
         |> Enum.map(fn coord ->
-          [x, y] = String.split(coord, ",")
-          {String.to_integer(x), String.to_integer(y)}
+          String.split(coord, ",")
+          |> Enum.map(&String.to_integer/1)
+          |> List.to_tuple()
         end)
         |> Enum.chunk_every(2, 1, :discard)
-        |> Enum.flat_map(fn [{x1, y1}, {x2, y2}] ->
-          if x1 == x2,
-            do: y1..y2 |> Enum.map(&{x1, &1}),
-            else: x1..x2 |> Enum.map(&{&1, y1})
+        |> Enum.flat_map(fn
+          [{x, y1}, {x, y2}] -> y1..y2 |> Enum.map(&{x, &1})
+          [{x1, y}, {x2, y}] -> x1..x2 |> Enum.map(&{&1, y})
         end)
       end)
       |> Map.new(&{&1, "#"})
 
-    {min_x, _} = Map.keys(cave) |> Enum.min_by(fn {x, _} -> x end)
-    {max_x, _} = Map.keys(cave) |> Enum.max_by(fn {x, _} -> x end)
+    {{min_x, _}, {max_x, _}} = Map.keys(cave) |> Enum.min_max_by(fn {x, _} -> x end)
     {_, max_y} = Map.keys(cave) |> Enum.max_by(fn {_, y} -> y end)
 
     {cave, min_x, max_x, max_y}
   end
 
-  def part1(args) do
-    {cave, min_x, max_x, max_y} = parse(args)
+  @start {500, 0}
 
-    {res, _} =
-      Stream.iterate(0, &(&1 + 1))
-      |> Enum.reduce_while({0, cave}, fn ix, {_, acc} ->
-        {next, new_sand} = fall(acc, {500, 0}, {min_x, max_x}, max_y)
-        {next, {ix, Map.put(acc, new_sand, "o")}}
-      end)
+  defp simulate(cave, min_x, max_x, max_y, infinite_floor \\ false) do
+    Stream.iterate(0, &(&1 + 1))
+    |> Enum.reduce_while(cave, fn _, acc ->
+      {next, new_sand} = fall(acc, @start, {min_x, max_x}, max_y, infinite_floor)
 
-    res
+      case next do
+        :cont -> {next, Map.put(acc, new_sand, "o")}
+        :halt -> {next, acc}
+      end
+    end)
+    |> Map.values()
+    |> Enum.count(&(&1 == "o"))
   end
 
-  def part2(args) do
-    {cave, min_x, max_x, max_y} = parse(args)
-
-    {res, _} =
-      Stream.iterate(0, &(&1 + 1))
-      |> Enum.reduce_while({0, cave}, fn ix, {_, acc} ->
-        {next, new_sand} = fall(acc, {500, 0}, {min_x, max_x}, max_y + 1, true)
-
-        next =
-          case new_sand do
-            {500, 0} -> :halt
-            _ -> next
-          end
-
-        {next, {ix, Map.put(acc, new_sand, "o")}}
-      end)
-
-    res + 1
-  end
-
-  defp fall(cave, {x, y}, {min, max}, max_y, infinite_floor \\ false) do
+  defp fall(cave, {x, y}, {min, max}, max_y, infinite_floor) do
     cond do
+      cave[@start] != nil -> {:halt, {x, y}}
       infinite_floor && y == max_y -> {:cont, {x, y}}
       y > max_y -> {:halt, {x, y}}
       cave[{x, y + 1}] == nil -> fall(cave, {x, y + 1}, {min, max}, max_y, infinite_floor)
