9166216b878c334c6d55f82fe131cb0a0daeb333
Sergio Diaz <sergio.diaz@pdq.com>
2023-12-06 08:29:55 -0600

    2023, day 6 brute force all the way

--- 2023 Elixir/lib/advent_of_code/day_06.ex
+++ 2023 Elixir/lib/advent_of_code/day_06.ex
@@ -1,7 +1,23 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn l -> String.split(l) |> Enum.drop(1) |> Enum.map(&String.to_integer/1) end)
+    |> Enum.zip()
+    |> Enum.map(fn {time, distance} ->
+      length(1..time |> Enum.filter(fn x -> (time - x) * x > distance end))
+    end)
+    |> Enum.reduce(1, &(&1 * &2))
   end
 
-  def part2(_args) do
+  def part2(args) do
+    [time, distance] =
+      args
+      |> String.split("\n", trim: true)
+      |> Enum.map(fn l ->
+        String.split(l) |> Enum.drop(1) |> Enum.join() |> String.to_integer()
+      end)
+
+    length(1..time |> Enum.filter(fn x -> (time - x) * x > distance end))
   end
 end
--- 2023 Elixir/test/advent_of_code/day_06_test.exs
+++ 2023 Elixir/test/advent_of_code/day_06_test.exs
@@ -3,19 +3,19 @@ defmodule AdventOfCode.Day06Test do
 
   import AdventOfCode.Day06
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "Time:      7  15   30
+Distance:  9  40  200"
     result = part1(input)
 
-    assert result
+    assert result == 288
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "Time:      7  15   30
+Distance:  9  40  200"
     result = part2(input)
 
-    assert result
+    assert result == 71503
   end
 end
