bcb4e49105dc16e82968ff17a948409932a41ebf
Sergio Diaz <sergio.diaz@pdq.com>
2023-12-08 10:55:24 -0600

    2023, day 8

--- 2023 Elixir/lib/advent_of_code/day_08.ex
+++ 2023 Elixir/lib/advent_of_code/day_08.ex
@@ -1,7 +1,51 @@
 defmodule AdventOfCode.Day08 do
-  def part1(_args) do
+  def part1(args) do
+    {dirs, map} = get_inpu(args)
+
+    Enum.reduce_while(Stream.cycle(dirs), {1, "AAA"}, fn dir, {c, next} ->
+      tmp = map[next][dir]
+
+      if(is_nil(tmp) || tmp == "ZZZ") do
+        {:halt, c}
+      else
+        {:cont, {c + 1, tmp}}
+      end
+    end)
   end
 
-  def part2(_args) do
+  def part2(args) do
+    {dirs, map} = get_inpu(args)
+
+    starts = Map.keys(map) |> Enum.filter(fn x -> String.ends_with?(x, "A") end)
+
+    starts
+    |> Enum.map(fn s ->
+      Enum.reduce_while(Stream.cycle(dirs), {1, s}, fn dir, {c, next} ->
+        tmp = map[next][dir]
+
+        if(is_nil(tmp) || String.ends_with?(tmp, "Z")) do
+          {:halt, c}
+        else
+          {:cont, {c + 1, tmp}}
+        end
+      end)
+    end)
+    |> Enum.reduce(&div(&1 * &2, Integer.gcd(&1, &2)))
+  end
+
+  defp get_inpu(args) do
+    [dir | tail] = args |> String.split("\n", trim: true)
+
+    dirs = String.codepoints(dir)
+
+    map =
+      tail
+      |> Enum.reduce(%{}, fn l, acc ->
+        [[node, l, r]] = Regex.scan(~r/(...) = \((...), (...)\)/, l, capture: :all_but_first)
+
+        Map.put(acc, node, %{"L" => l, "R" => r})
+      end)
+
+    {dirs, map}
   end
 end
--- 2023 Elixir/test/advent_of_code/day_08_test.exs
+++ 2023 Elixir/test/advent_of_code/day_08_test.exs
@@ -3,19 +3,30 @@ defmodule AdventOfCode.Day08Test do
 
   import AdventOfCode.Day08
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "LLR
+
+AAA = (BBB, BBB)
+BBB = (AAA, ZZZ)
+ZZZ = (ZZZ, ZZZ)"
     result = part1(input)
 
-    assert result
+    assert result == 6
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"
     result = part2(input)
 
-    assert result
+    assert result == 6
   end
 end
