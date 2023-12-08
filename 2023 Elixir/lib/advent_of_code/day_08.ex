b2798a01d77962dde955c49d936f55c44c22e694
Sergio Diaz <sergio.diaz@pdq.com>
2023-12-08 11:02:48 -0600

    2023, day 8 cleanup

--- 2023 Elixir/lib/advent_of_code/day_08.ex
+++ 2023 Elixir/lib/advent_of_code/day_08.ex
@@ -1,41 +1,16 @@
 defmodule AdventOfCode.Day08 do
   def part1(args) do
-    {dirs, map} = get_inpu(args)
-
-    Enum.reduce_while(Stream.cycle(dirs), {1, "AAA"}, fn dir, {c, next} ->
-      tmp = map[next][dir]
-
-      if(is_nil(tmp) || tmp == "ZZZ") do
-        {:halt, c}
-      else
-        {:cont, {c + 1, tmp}}
-      end
-    end)
+    {dirs, map} = get_input(args)
+    solve(dirs, map, ["AAA"], "ZZZ")
   end
 
   def part2(args) do
-    {dirs, map} = get_inpu(args)
-
-    starts = Map.keys(map) |> Enum.filter(fn x -> String.ends_with?(x, "A") end)
-
-    starts
-    |> Enum.map(fn s ->
-      Enum.reduce_while(Stream.cycle(dirs), {1, s}, fn dir, {c, next} ->
-        tmp = map[next][dir]
-
-        if(is_nil(tmp) || String.ends_with?(tmp, "Z")) do
-          {:halt, c}
-        else
-          {:cont, {c + 1, tmp}}
-        end
-      end)
-    end)
-    |> Enum.reduce(&div(&1 * &2, Integer.gcd(&1, &2)))
+    {dirs, map} = get_input(args)
+    solve(dirs, map, Map.keys(map) |> Enum.filter(fn x -> String.ends_with?(x, "A") end), "Z")
   end
 
-  defp get_inpu(args) do
+  defp get_input(args) do
     [dir | tail] = args |> String.split("\n", trim: true)
-
     dirs = String.codepoints(dir)
 
     map =
@@ -47,5 +22,21 @@ defmodule AdventOfCode.Day08 do
       end)
 
     {dirs, map}
+  end
+
+  defp solve(dirs, map, starts, finish) do
+    starts
+    |> Enum.map(fn s ->
+      Enum.reduce_while(Stream.cycle(dirs), {1, s}, fn dir, {c, next} ->
+        tmp = map[next][dir]
+
+        if(String.ends_with?(tmp, finish)) do
+          {:halt, c}
+        else
+          {:cont, {c + 1, tmp}}
+        end
+      end)
+    end)
+    |> Enum.reduce(&div(&1 * &2, Integer.gcd(&1, &2)))
   end
 end
