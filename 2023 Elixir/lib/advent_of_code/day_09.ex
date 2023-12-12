18081d2482574000aeb7661a23eb752458aa055e
Sergio Diaz <sergio.diaz@pdq.com>
2023-12-11 18:12:29 -0600

    2023, day 9

--- 2023 Elixir/lib/advent_of_code/day_09.ex
+++ 2023 Elixir/lib/advent_of_code/day_09.ex
@@ -1,7 +1,48 @@
 defmodule AdventOfCode.Day09 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn l -> String.split(l, " ") |> Enum.map(&String.to_integer/1) end)
+    |> Enum.map(fn line ->
+      Enum.reduce_while(
+        Stream.iterate(1, &(&1 + 1)),
+        {line, Enum.take(line, -1)},
+        fn _, {current, list} ->
+          if Enum.all?(current, fn x -> x == 0 end) do
+            {:halt, list}
+          else
+            next = diffs(current)
+            {:cont, {next, [Enum.at(next, -1) | list]}}
+          end
+        end
+      )
+      |> Enum.sum()
+    end)
+    |> Enum.sum()
   end
 
-  def part2(_args) do
+  defp diffs([s, f]), do: [f - s]
+  defp diffs([f, s | tail]), do: [s - f | diffs([s | tail])]
+
+  def part2(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn l -> String.split(l, " ") |> Enum.map(&String.to_integer/1) end)
+    |> Enum.map(fn line ->
+      Enum.reduce_while(
+        Stream.iterate(1, &(&1 + 1)),
+        {line, Enum.take(line, 1)},
+        fn _, {current, list} ->
+          if Enum.all?(current, fn x -> x == 0 end) do
+            {:halt, list}
+          else
+            next = diffs(current)
+            {:cont, {next, [Enum.at(next, 0) | list]}}
+          end
+        end
+      )
+      |> Enum.reduce(fn x, acc -> x - acc end)
+    end)
+    |> Enum.sum()
   end
 end
