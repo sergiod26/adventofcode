0092c04705e5988a83ee035656eb460775fec348
Sergio Diaz <sergio.diaz@pdq.com>
2023-12-12 09:52:01 -0600

    2023, day 12 part1

--- 2023 Elixir/lib/advent_of_code/day_12.ex
+++ 2023 Elixir/lib/advent_of_code/day_12.ex
@@ -1,5 +1,40 @@
 defmodule AdventOfCode.Day12 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(fn l ->
+      [springs, numbers] = String.split(l, " ", trim: true)
+      springs = String.codepoints(springs)
+      numbers = String.split(numbers, ",") |> Enum.map(&String.to_integer/1)
+
+      {springs, numbers}
+
+      combinations(springs, Enum.sum(numbers))
+      |> Enum.filter(fn comb -> count_defects(List.to_string(comb)) == numbers end)
+      |> length()
+    end)
+    |> Enum.sum()
+  end
+
+  defp combinations([head], _max) do
+    if head == "?",
+      do: [["."], ["#"]],
+      else: [[head]]
+  end
+
+  defp combinations([head | tail], max) do
+    rest = combinations(tail, max)
+
+    if head == "?" do
+      Enum.map(rest, fn sub -> ["." | sub] end) ++
+        Enum.map(rest, fn sub -> ["#" | sub] end)
+    else
+      Enum.map(rest, fn sub -> [head | sub] end)
+    end
+  end
+
+  defp count_defects(str) do
+    Regex.scan(~r/(#+)/, str) |> Enum.map(fn [_, x] -> String.codepoints(x) |> length() end)
   end
 
   def part2(_args) do
--- 2023 Elixir/test/advent_of_code/day_12_test.exs
+++ 2023 Elixir/test/advent_of_code/day_12_test.exs
@@ -3,12 +3,16 @@ defmodule AdventOfCode.Day12Test do
 
   import AdventOfCode.Day12
 
-  @tag :skip
   test "part1" do
-    input = nil
+    input = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1"
     result = part1(input)
 
-    assert result
+    assert result == 21
   end
 
   @tag :skip
