bdf43450e6e69921035d42f5838c521a7a79dbbb
Sergio Diaz <sergio.diaz@pdq.com>
2023-12-19 09:53:33 -0600

    2023, day 19 part1

--- 2023 Elixir/lib/advent_of_code/day_19.ex
+++ 2023 Elixir/lib/advent_of_code/day_19.ex
@@ -1,6 +1,72 @@
 defmodule AdventOfCode.Day19 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> String.trim()
+    |> String.split("\n\n", trim: true)
+    |> tap(fn x -> dbg(length(x)) end)
+    |> then(fn [workflows, ratings] ->
+      workflows =
+        String.split(workflows, "\n")
+        |> Enum.map(fn workflow ->
+          Regex.scan(~r/([^{]+){(.+)}/, workflow, capture: :all_but_first)
+          |> then(fn [[name, str_rules]] ->
+            rules =
+              String.split(str_rules, ",")
+              |> Enum.map(fn str_rule -> rule(String.split(str_rule, ":")) end)
+
+            {name, rules}
+          end)
+        end)
+        |> Map.new()
+
+      ratings =
+        String.split(ratings, "\n")
+        |> Enum.map(fn rating ->
+          String.replace(rating, ["{", "}"], "")
+          |> String.split(",")
+          |> Enum.map(fn rating ->
+            [cat, val] = String.split(rating, "=")
+            {cat, String.to_integer(val)}
+          end)
+          |> Map.new()
+        end)
+
+      {workflows, ratings}
+    end)
+    |> then(fn {workflows, ratings} ->
+      Enum.filter(ratings, fn rating -> solve(workflows, rating, "in") end)
+      |> Enum.flat_map(fn m -> Map.values(m) end)
+      |> Enum.sum()
+    end)
   end
+
+  defp solve(_, _, "A"), do: true
+  defp solve(_, _, "R"), do: false
+
+  defp solve(workflows, rating, label) do
+    solve(workflows, rating, apply_rule(workflows[label], rating))
+  end
+
+  defp apply_rule(workflow, rating) do
+    Enum.reduce_while(workflow, nil, fn {label, foo}, _ ->
+      next = foo.(rating[label])
+
+      if is_nil(next), do: {:cont, nil}, else: {:halt, next}
+    end)
+  end
+
+  defp rule([condition, label]) do
+    [[cat, sign, rating]] = Regex.scan(~r/(.)([<>])(\d+)/, condition, capture: :all_but_first)
+
+    comp =
+      if sign == "<",
+        do: fn x -> if x < String.to_integer(rating), do: label end,
+        else: fn x -> if x > String.to_integer(rating), do: label end
+
+    {cat, comp}
+  end
+
+  defp rule([label]), do: {nil, fn _ -> label end}
 
   def part2(_args) do
   end
--- 2023 Elixir/test/advent_of_code/day_19_test.exs
+++ 2023 Elixir/test/advent_of_code/day_19_test.exs
@@ -3,19 +3,31 @@ defmodule AdventOfCode.Day19Test do
 
   import AdventOfCode.Day19
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input "px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
 
-    assert result
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}"
+
+  test "part1" do
+    result = part1(@input)
+    assert result == 19114
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
-
-    assert result
+    result = part2(@input)
+    assert result == 167_409_079_868_000
   end
 end
