cfbdc9dfdd9dc179e25f138d81718a3a1e32c941
Sergio Diaz <sergio.diaz@pdq.com>
2023-12-28 14:00:04 -0600

    2023, day 25 only works with test

--- 2023 Elixir/lib/advent_of_code/day_25.ex
+++ 2023 Elixir/lib/advent_of_code/day_25.ex
@@ -1,7 +1,76 @@
 defmodule AdventOfCode.Day25 do
-  def part1(_args) do
+  # Attempt of Karger's algorithm
+
+  def part1(args) do
+    edges =
+      String.split(args, "\n", trim: true)
+      |> Enum.reduce([], fn line, acc ->
+        [h | t] = String.split(line, [":", " "], trim: true)
+        Enum.map(t, fn con -> {h, con} end) ++ acc
+      end)
+
+    g = Graph.new() |> Graph.add_edges(edges)
+
+    reduce_graph(g)
+    |> Graph.vertices()
+    |> Enum.map(fn v -> String.split(v, " ") |> length() end)
+    |> Enum.reduce(1, fn l, acc -> l * acc end)
+  end
+
+  defp min_cut(g, 2), do: g
+
+  defp min_cut(g, _) do
+    %Graph.Edge{v1: a, v2: b} = Graph.edges(g) |> Enum.random()
+
+    ab = "#{a} #{b}"
+
+    tmp_edges =
+      Graph.neighbors(g, b)
+      |> Enum.map(fn v -> if v != a, do: {ab, v} end)
+      |> Enum.filter(fn x -> !is_nil(x) end)
+
+    gg =
+      Graph.replace_vertex(g, a, ab)
+      |> Graph.delete_vertex(b)
+      |> Graph.add_edges(tmp_edges)
+
+    min_cut(gg, length(Graph.vertices(gg)))
+  end
+
+  defp reduce_graph(g) do
+    graph = min_cut(g, 0)
+    cuts = with_cache({g, graph}, fn -> count_cuts(g, graph) end)
+
+    if cuts == 3 do
+      graph
+    else
+      reduce_graph(g)
+    end
+  end
+
+  defp count_cuts(g, graph) do
+    nodes =
+      Graph.vertices(graph)
+      |> List.first()
+      |> String.split(" ")
+
+    nodes
+    |> Enum.reduce([], fn v, acc ->
+      (Graph.neighbors(g, v)
+       |> Enum.map(fn x ->
+         if !(x in nodes && v in nodes) && {x, v} not in acc && {v, x} not in acc, do: {v, x}
+       end)) ++ acc
+    end)
+    |> Enum.filter(fn x -> !is_nil(x) end)
+    |> length()
   end
 
   def part2(_args) do
+  end
+
+  defp with_cache(key, fun) do
+    with nil <- Process.get(key) do
+      fun.() |> tap(&Process.put(key, &1))
+    end
   end
 end
--- 2023 Elixir/test/advent_of_code/day_25_test.exs
+++ 2023 Elixir/test/advent_of_code/day_25_test.exs
@@ -3,19 +3,28 @@ defmodule AdventOfCode.Day25Test do
 
   import AdventOfCode.Day25
 
-  @tag :skip
-  test "part1" do
-    input = nil
-    result = part1(input)
+  @input "jqt: rhn xhk nvd
+rsh: frs pzl lsr
+xhk: hfx
+cmg: qnr nvd lhk bvb
+rhn: xhk bvb hfx
+bvb: xhk hfx
+pzl: lsr hfx nvd
+qnr: nvd
+ntq: jqt hfx bvb xhk
+nvd: lhk
+lsr: lhk
+rzs: qnr cmg lsr rsh
+frs: qnr lhk lsr"
 
-    assert result
+  test "part1" do
+    result = part1(@input)
+    assert result == 54
   end
 
   @tag :skip
   test "part2" do
-    input = nil
-    result = part2(input)
-
+    result = part2(@input)
     assert result
   end
 end
